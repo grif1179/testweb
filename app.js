db4b5177c1623dc8d7789c65744c24482d239f20
Grant Park <grif1179@gmail.com>
2017-05-08 21:22:59 -0400

    fixed the timer fns

--- app.js
+++ app.js
@@ -1,98 +1,149 @@
-var currentDate;
-var currDateStr;
-var dateObj;
 var workTime;
 var workMin;
 var workSec;
 var breakTime;
 var breakMin;
 var breakSec;
-var stop;
-var workInt;
-var breakInt;
-
-$('#startTimer').click(function(){
-    breakTime   = $('#breakTime').val();
-    workTime    = $('#workTime').val();
-    workMin     = workTime;
-    workSec     = 0;
-    breakMin    = breakTime;
-    breakSec    = 0;
-    stop        = false;
-
-    workInt     = setTimeout(decWork(),1000);
-
+var stopWork;
+var stopBreak;
+var currentPhase = "Work";
+var init = 0;
+$('#start_pause').click(function(){
+  if(init === 0)
+  {
+      $('#startTimer').css('display','none');
+      $('#pauseTimer').css('display','block');
+      $('#resetTimer').css('display','block');
+      breakTime   = $('#breakTime').val();
+      workTime    = $('#workTime').val();
+      workMin     = workTime;
+      workSec     = 0;
+      breakMin    = breakTime;
+      breakSec    = 0;
+      stopWork    = false;
+      decWork();
+  }//first time start is pressed
 });
+
 
 $('#pauseTimer').click(function()
 {
-  stop = true;
+  stopWork  = true;
+  stopBreak = true;
 });
 
 function decWork()
 {
-   if(!stop)
+   if(!stopWork)
    {
-     if(workMin == 0 && workSec == 0 )
-     {
-       clearTimeout(workInt);
-       setTimeout(decBreak(),1000);
-     }
-     else if(workMin > 0)
-     {
-       if(workSec == 0)
+      setTimeout(function(){
+       if(workMin == 0 && workSec == 0 )
        {
-         workSec = 59;
-         workMin--;
+        breakTime   = $('#breakTime').val();
+        workTime    = $('#workTime').val();
+        workMin     = workTime;
+        workSec     = 0;
+        breakMin    = breakTime;
+        breakSec    = 0;
+        stopWork = true;
+        stopBreak = false;
+        decBreak();
+       }
+       else if(workMin > 0)
+       {
+         if(workSec == 0)
+         {
+           workSec = 59;
+           workMin--;
+           workDisplay();
+         }
+         else
+         {
+            workSec--;
+            workDisplay();
+         }
+       }
+       else if(workMin == 0 && workSec > 0)
+       {
+         workSec--;
          workDisplay();
        }
-       else
-       {
-          workSec--;
-          workDisplay();
-       }
-     }
-     else if(workMin == 0 && workSec > 0)
-     {
-       workSec--;
-       workDisplay();
-     }
-     setTimeout(decWork,1000);
+       decWork();
+     },1000);//end of setTimeout fns
    }//end stop
 }//end decWork
 
-
 function decBreak()
 {
-   if(!stop)
+   if(!stopBreak)
    {
-     if(breakMin == 0 && breakSec == 0 )
-     {
-       clearTimeout(breakInt)
-       setTimeout(decWork(),1000);
-     }
-     else if(breakMin > 0)
-     {
-       if(breakSec == 0)
+      setTimeout(function(){
+       if(breakMin == 0 && breakSec == 0 )
        {
-         breakSec = 59;
-         breakMin--;
+          breakTime   = $('#breakTime').val();
+          workTime    = $('#workTime').val();
+          workMin     = workTime;
+          workSec     = 0;
+          breakMin    = breakTime;
+          breakSec    = 0;
+         stopWork = false;
+         stopBreak = true;
+         decWork();
+       }
+       else if(breakMin > 0)
+       {
+         if(breakSec == 0)
+         {
+           breakSec = 59;
+           breakMin--;
+           breakDisplay();
+         }
+         else
+         {
+            breakSec--;
+            breakDisplay();
+         }
+       }
+       else if(breakMin == 0 && breakSec > 0)
+       {
+         breakSec--;
          breakDisplay();
        }
-       else
-       {
-          breakSec--;
-          breakDisplay();
-       }
-     }
-     else if(breakMin == 0 && breakSec > 0)
-     {
-       breakSec--;
-       breakDisplay();
-     }
-     setTimeout(decBreak,1000);
+       decBreak();
+     },1000);//end of setTimeout fns
    }//end stop
 }
+// function decBreak()
+// {
+//    if(!stop)
+//    {
+//      if(breakMin == 0 && breakSec == 0 )
+//      {
+//        clearTimeout(breakInt)
+//        setTimeout(decWork(),1000);
+//      }
+//      else if(breakMin > 0)
+//      {
+//        if(breakSec == 0)
+//        {
+//          breakSec = 59;
+//          breakMin--;
+//          breakDisplay();
+//        }
+//        else
+//        {
+//           breakSec--;
+//           breakDisplay();
+//        }
+//      }
+//      else if(breakMin == 0 && breakSec > 0)
+//      {
+//        breakSec--;
+//        breakDisplay();
+//      }
+//      setTimeout(decBreak,1000);
+//    }//end stop
+// }
 
 var workDisplay = function()
 {
